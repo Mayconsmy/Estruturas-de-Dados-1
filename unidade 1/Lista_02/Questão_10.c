50ad75951a44ec06bcca959f41c76d84cb5bf8b1
Mayconsmy <1mayconsm2@gmail.com>
2025-03-08 21:22:35 -0300

    q10

---
+++ unidade 1/Lista_02/Questão_10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    
+    int valor = 10, *temp, soma = 0;
+    temp = &valor;
+    *temp = 20;
+    temp = &soma;
+    *temp = valor;
+    printf("valor = %d, *temp = %d, soma = %d\n", valor, *temp, soma);
+
+    return 0;
+}
