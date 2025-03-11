37e823be06726926cbdba25c91d4121ae12c577b
Mayconsmy <1mayconsm2@gmail.com>
2025-03-11 20:28:03 -0300

    q1

---
+++ unidade 1/Lista_01/Questão_01.C
@@ -0,0 +1,3 @@
+/*R: Os ponteiros são variáveis que armazenam endereços de memórias. Serve para
+possibilitar a manipulação eficiente da memória, pois permite o acesso e a modificação direta
+de variáveis*/
