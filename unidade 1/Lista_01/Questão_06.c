a4f325cd61fb12eb6ec49eb8c815cae777c5c9aa
Mayconsmy <1mayconsm2@gmail.com>
2025-03-11 20:30:50 -0300

    q6

---
+++ unidade 1/Lista_01/Questão_06.c
@@ -0,0 +1,4 @@
+/* R: Não está correto, pois o scanf é usado para capturar valores da entrada padrão
+(teclado) e armazená-los em variáveis e o *px significa o valor armazenado no endereço que
+px aponta, ou seja, *px acessa diretamente x, fornecendo assim o valor inteiro e não o seu
+endereço. */
